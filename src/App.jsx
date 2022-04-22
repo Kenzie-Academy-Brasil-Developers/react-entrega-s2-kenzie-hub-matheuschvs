c737baf3bfe68d6773b6f30ce74e0e2f5cbd5584
Matheus C <matheuschaves21@gmail.com>
2022-04-22 10:06:13 -0300

    feat: implement global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './reset.css';
-import './App.css';
-
 import { Router } from './routes';
+import { GlobalStyle } from './styles/global'
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <GlobalStyle />
       <Router />
-    </div>
+    </>
   );
 }
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,7 +5,7 @@ import { Home } from '../pages/Home';
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Home />} />
     </Routes>
   )
 }
